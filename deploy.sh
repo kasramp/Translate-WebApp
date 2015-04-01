c32cc9dd6ec65fcb42bfee1800111613ff655cb2
kasra_mp <kasra_mp@yahoo.com>
2015-04-01 15:28:12 +0800

    Replace cp with rsync to copy only changed files to deploy

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,10 @@ else
 	HARD_PATH=$1;
 fi
 #OVERWRITES IF CONTENTS EXIST OR CREATE A NEW FOLDER IF NOT EXITS
-mkdir -p $HARD_PATH$DIRECTORY_NAME;
-cp -r * $HARD_PATH$DIRECTORY_NAME;
+#mkdir -p $HARD_PATH$DIRECTORY_NAME;
+#cp -r * $HARD_PATH$DIRECTORY_NAME;
+#REPLACE WITH RSYNC
+rsync -av $CURRENT_PATH $HARD_PATH;
 #GET CP RETURN VALUE, WHICH HOLDS IN LAST VARIABLE, THE COMMAND SHOWS GET LAST VARIABLE IN BASH
 #echo $?; 
 if [ $? -eq 0 ]; then
@@ -17,7 +19,4 @@ if [ $? -eq 0 ]; then
 else
 	echo "Deployed failed, try with root user!"
 fi
-
-
-
 
